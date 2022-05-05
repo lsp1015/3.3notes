968f5ac7e8593cb1ba7db9384e14ac9237319f53
lsp1015 <kdns@fresnou.org>
2022-05-05 13:47:25 -0700

    3.13

---
+++ index.js
@@ -0,0 +1,34 @@
+const cardList = document.querySelector( '.cardList');
+
+buildBoard();
+
+let interval = setInterval(function(){
+    addCard(cardList.children.length + 1)
+},2000);
+
+cardList.addEventListener('click', function(e){
+    console.log(e.target);
+    if (e.target.matches('.cardList')){
+        return
+    }
+    if (e.target.classList.contains('active')){
+        e.target.classList.remove('active');
+        e.target.classList.add('inactive');
+        return
+    }
+    e.target.remove();
+})
+
+function addCard(value){
+    let card = document.createElement( 'div');
+    card.classList.add('card');
+    card.classList.add('active');
+    card.innerHTML = value;
+    cardList.appendChild(card);
+}
+
+function buildBoard(){
+    for(let i=0; i<12; i++){
+        addCard('starter');
+    }
+}
